9191f54f55fbda3726b68c1dcc82966850d7a5d7
Nathan Towe <nathanwtowe89@gmail.com>
2023-10-28 13:59:33 -0400

    Adding 404 page view

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
+
 require('dotenv').config()
 const express = require('express')
 const app = express()
 
-//middleware
+//middleware //view engine
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
-
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 
+
+//places controller
 app.use('/places', require('./controllers/places'))
 
+//routes
 app.get('/', (req, res) => {
     res.render('home')
 })
 
+//404 page
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
+    res.render('error404')
 })
 
+//listen
 app.listen(process.env.PORT)
 
