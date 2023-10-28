59f2fd6cd8198ac37f608b2467946649f35bf2ac
Nathan Towe <nathanwtowe89@gmail.com>
2023-10-28 18:41:14 -0400

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,21 +1,8 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 //INDEX
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/thai.bmp'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/coffee.bmp'
-      }]
-      
     res.render('places/index', {places})
 })
 
@@ -26,7 +13,19 @@ router.get('/new', (req, res) => {
 
 //CREATE
 router.post('/', (req, res) => {
-  console.log(req.body)
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
 })
+
 
 module.exports = router
