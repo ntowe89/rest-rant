d12ce13533b405bd31a39149bcaf2cb273c30d9a
Nathan Towe <nathanwtowe89@gmail.com>
2023-10-30 21:08:28 -0400

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -11,6 +11,20 @@ router.get('/new', (req, res) => {
     res.render('places/new')
 })
 
+//SHOW
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id] })
+  }
+})
+
 //CREATE
 router.post('/', (req, res) => {
   if (!req.body.pic) {
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -2,12 +2,16 @@ const React = require('react')
 const Def = require('../layouts/default')
 
 function index (data) {
-    let placesFormatted = data.places.map((place) => {
+    let placesFormatted = data.places.map((place, index) => {
       return (
         <div className="container">
           <div className="row">
             <div className="col-sm-6">
-            <h2>{place.name}</h2>
+            <h2>
+              <a href={`/places/${index}`}>
+                {place.name}
+              </a>
+            </h2>
             <p className="text-center">
              {place.cuisines}
             </p>
