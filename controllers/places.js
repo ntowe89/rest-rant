454607986de9d5cd2388ac2e90c432b6958b5532
Nathan Towe <nathanwtowe89@gmail.com>
2023-10-30 21:49:17 -0400

    Added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -56,6 +56,22 @@ router.delete('/:id', (req, res) => {
   }
 })
 
+//EDIT
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+    res.render('places/edit', { place: places[id] })
+  }
+})
+
+
+
 
 
 module.exports = router
