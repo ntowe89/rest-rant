e36c70a57149676c30a67e374ebe1c5864de4ea0
Nathan Towe <nathanwtowe89@gmail.com>
2023-11-01 22:10:30 -0400

    finished implementing the Mongoose ODM

--- views/layouts/Default.jsx
+++ views/layouts/Default.jsx
@@ -5,7 +5,7 @@ function Def (html) {
         <html>
             <head>
                 <title>Title</title>
-                {/* <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"/> */}
+                {<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossOrigin="anonymous"/>}
                 <link rel="stylesheet" href="/css/style.css"/>
             </head>
             <body>
--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -6,7 +6,7 @@ function edit_form (data) {
         <Def>
           <main>
             <h1>Edit Place</h1>
-            <form method="POST" action={`/places/${data.place.id}?_method=put`}>
+            <form method="POST" action={`/places/${data.id}?_method=put`}>
                 <div className="form-group">
                     <label htmlFor="name">Place Name</label>
                     <input 
